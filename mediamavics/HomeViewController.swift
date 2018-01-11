3c69dba6818d671bd7721e2e47286a13e351a2c9
Johnathan Chen <johnathanchenn@outlook.com>
2018-01-10 18:44:05 -0800

    init

---
+++ mediamavics/HomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeViewController.swift
+//  mediamavics
+//
+//  Created by Johnathan Chen on 12/26/17.
+//  Copyright © 2017 JCSwifty. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
+
+
+
