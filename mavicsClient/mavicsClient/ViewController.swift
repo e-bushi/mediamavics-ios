53b9996396380f63df51aaa6f703f766186082a0
Johnathan Chen <johnathanchenn@outlook.com>
2018-01-13 13:55:31 -0800

    Carthage update

---
+++ mavicsClient/mavicsClient/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  mavicsClient
+//
+//  Created by Johnathan Chen on 1/13/18.
+//  Copyright © 2018 JCSwifty. All rights reserved.
+//
+
+import UIKit
+import Realm
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
