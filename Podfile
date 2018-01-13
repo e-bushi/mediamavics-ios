924ac0ff4afee35b98aa3a87e4f1214a931a8792
Johnathan Chen <johnathanchenn@outlook.com>
2018-01-12 17:42:52 -0800

    set up server

---
+++ Podfile
@@ -0,0 +1,3 @@
+use_frameworks!
+target 'mediamavics'
+pod 'RealmSwift'
---
+++ mediamavics/Model/ChannelSelection.swift
@@ -0,0 +1,10 @@
+//
+//  ChannelSelection.swift
+//  mediamavics
+//
+//  Created by Johnathan Chen on 1/12/18.
+//  Copyright © 2018 JCSwifty. All rights reserved.
+//
+
+import Foundation
+import RealmSwift
