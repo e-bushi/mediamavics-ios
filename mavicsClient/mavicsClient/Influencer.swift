bcc6c605cc518dea65afa1bcf721b30b3a24cbeb
Chris Mauldin <Chrislovemauldin@gmail.com>
2018-01-14 11:47:38 -0800

    update Influencer model properties

--- mavicsClient/mavicsClient/Influencer.swift
+++ mavicsClient/mavicsClient/Influencer.swift
@@ -11,8 +11,8 @@ import Foundation
 struct Influencer {
     let name: String
     let age: Int
-//    let location: String
-//    let instagramFollowing: Int
-//    let facebookFollowing: Int
-//    let youtubeFollowing: Int
+    let location: String
+    let instagramFollowing: Int
+    let facebookFollowing: Int
+    let youtubeFollowing: Int
 }
